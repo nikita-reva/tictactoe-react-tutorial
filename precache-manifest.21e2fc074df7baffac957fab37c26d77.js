66d51a3dac977cf662c11f5849256d5cd1c007bc
NikoReva <57528503+NikoReva@users.noreply.github.com>
2020-11-03 13:35:16 +0100

    Updates

---
+++ precache-manifest.21e2fc074df7baffac957fab37c26d77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "008452573d13d78210f6bed89bda5ed6",
+    "url": "/tictactoe-react-tutorial/index.html"
+  },
+  {
+    "revision": "1a89dc0a35a3b4fd82f6",
+    "url": "/tictactoe-react-tutorial/static/css/main.0d1f71d3.chunk.css"
+  },
+  {
+    "revision": "dfdbd973b4b1cc8ebf19",
+    "url": "/tictactoe-react-tutorial/static/js/2.8ea157b5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tictactoe-react-tutorial/static/js/2.8ea157b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a89dc0a35a3b4fd82f6",
+    "url": "/tictactoe-react-tutorial/static/js/main.f14fac60.chunk.js"
+  },
+  {
+    "revision": "f9906f3ec3cd30c3a0dc",
+    "url": "/tictactoe-react-tutorial/static/js/runtime-main.5912a3a7.js"
+  }
+]);
